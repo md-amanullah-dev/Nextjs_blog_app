d93c271c3fcc0cec54fac733f05683d479374962
AMANULLAH023HY <amana170829@gmail.com>
2024-07-12 15:22:13 +0530

    blog post api add in database

---
+++ app/api/blog/route.js
@@ -0,0 +1,50 @@
+import connectDB from "@/lib/config/db";
+import { NextResponse } from "next/server";
+import {writeFile} from 'fs/promises'
+import { write } from "fs";
+import BlogModel from "@/lib/models/blogModel";
+// Database connection call
+const LoadDB = async () =>{
+    await connectDB()
+}
+LoadDB();
+// test server running 
+export async function GET(response){
+    console.log("Blog Get Hit");
+    return NextResponse.json({
+        message:"Api work",
+
+    })
+}
+
+export async function POST(request,response){
+const formData = await request.formData();
+const timestamp = Date.now();
+
+const image = formData.get('image');
+const imageByteData = await image.arrayBuffer();
+const buffer = Buffer.from(imageByteData);
+const path = `./public/${timestamp}_${image.name}`;
+await writeFile(path,buffer);
+const imageURL = `/${timestamp}_${image.name}`;
+
+const blogData = {
+    title:`${formData.get('title')}`,
+    description:`${formData.get('description')}`,
+    category:`${formData.get('category')}`,
+    author:`${formData.get('author')}`,
+    image:`${imageURL}`,
+    author_image:`${formData.get('Profile_Image')}`,
+    
+}
+
+await BlogModel.create(blogData);
+
+console.log(blogData)
+console.log("Blog saved")
+return NextResponse.json({
+    success:true,
+    message:"Blog Added Successfully!",
+    
+})
+}
